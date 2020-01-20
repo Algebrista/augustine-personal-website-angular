cc4ca1d287aaa3ae37e2011515882ab9ee1b6c76
algebrista <augustinematturi@outlook.com>
2020-01-20 16:53:56 +0100

    ADDED website structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    HomeComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
